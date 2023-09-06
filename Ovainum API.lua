c938b97a47573f5a8d4684bd783af8f2c15f07fd
Crystal-Cave <84108554+Crystal-Cave@users.noreply.github.com>
2023-09-06 15:27:34 -0400

    Added "FixMessage" Function

--- Ovainum API.lua
+++ Ovainum API.lua
@@ -7,20 +7,20 @@ function Ovainum:GetNearbyPlayers(plr, radius)
 	local T_players = {}
 	local Plr_Char
 	local cout = 0
-	
+
 	if plr.Character ~= nil then
 		Plr_Char = plr.Character
 	else
 		return {false}
 	end
-	
+
 	for _, model in E_workspace:GetDescendants() do
 		if model:IsA("Model") and players:GetPlayerFromCharacter(model) ~= nil and players:GetPlayerFromCharacter(model) ~= plr and Ovainum:IsInRadiusOf(Plr_Char, model, radius) == true then
 			table.insert(T_players, players:GetPlayerFromCharacter(model))
 			cout += 1
 		end 
 	end
-	
+
 	return {cout, T_players}
 end
 
@@ -30,6 +30,25 @@ function Ovainum:IsInRadiusOf(orgin, thing, radius)
 	else
 		return false
 	end
+end
+
+function Ovainum:FixMessage(text)
+	local FixedMessage
+	local int = 0
+	local int2 = 0
+	for _, chunck in string.split(text, "<") do
+		int += 1
+		if int == 2 then
+			for _, complex in string.split(chunck, ">") do
+				int2 += 1
+				if int2 == 2 then
+					FixedMessage = complex
+				end
+			end
+		end
+	end
+
+	return FixedMessage
 end
 
 return Ovainum
