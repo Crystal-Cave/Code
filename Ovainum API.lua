0768700d535365b52e900bfe32d4b7feb757fa62
Crystal-Cave <84108554+Crystal-Cave@users.noreply.github.com>
2023-09-10 16:06:26 -0400

    Added TableToString()

--- Ovainum API.lua
+++ Ovainum API.lua
@@ -51,4 +51,54 @@ function Ovainum:FixMessage(text)
 	return FixedMessage
 end
 
+function Ovainum:GetMaxIterations(tbl)
+	local iter = 0
+	for _, v in tbl do
+		iter += 1
+	end
+	return iter
+end
+
+function Ovainum:TableToString(tbl, level)
+	local Completed_String = "{"
+	local iteration = 0
+	for i, v in tbl do
+		iteration += 1
+		if iteration ~= Ovainum:GetMaxIterations(tbl) then
+			if type(i) == "number" then
+				Completed_String = Completed_String.."["..tostring(i).."] = "
+			elseif type(i) == "string" then
+				Completed_String = Completed_String..'["'..tostring(i)..'"] = '
+			end
+
+			if type(v) == "number" then
+				Completed_String = Completed_String..tostring(v)..", "
+			elseif type(v) == "string" then
+				Completed_String = Completed_String..'"'..tostring(v)..'", '
+			elseif type(v) == "table" then
+				Completed_String = Completed_String..Ovainum:TableToString(v, 2)..", "
+			end
+		else
+			if type(i) == "number" then
+				Completed_String = Completed_String.."["..tostring(i).."] = "
+			elseif type(i) == "string" then
+				Completed_String = Completed_String..'["'..tostring(i)..'"] = '
+			end
+
+			if type(v) == "number" then
+				Completed_String = Completed_String..tostring(v).."}"
+			elseif type(v) == "string" then
+				Completed_String = Completed_String..'"'..tostring(v)..'"}'
+			elseif type(v) == "table" then
+				Completed_String = Completed_String..Ovainum:TableToString(v, 2).."}"
+			end
+		end
+	end
+	if level == 1 then
+		return "return "..Completed_String
+	elseif level == 2 then
+		return Completed_String
+	end
+end
+
 return Ovainum
